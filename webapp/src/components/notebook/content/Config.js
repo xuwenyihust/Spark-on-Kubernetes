30889601ee6861af88280f5342f94119ca3ce531
xuwenyihust <wenyixu101@gmail.com>
2024-08-13 16:55:17 +0800

    Update Notebook component to add Config content type support (#319)

---
+++ webapp/src/components/notebook/content/Config.js
@@ -0,0 +1,17 @@
+
+import { Box, Typography } from '@mui/material';
+
+function Config({ }) {
+
+  return (
+    <Box sx={{
+      marginTop: 5,
+      marginRight: 5,
+      marginLeft: 2,
+    }}>
+      <Typography variant="h4"> Configuration </Typography>
+    </Box>
+  );
+}
+
+export default Config;
