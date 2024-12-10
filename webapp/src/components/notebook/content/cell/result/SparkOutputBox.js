e066b36effc908423e984578e383683d5d2eed70
Wenyi Xu <wenyixu101@gmail.com>
2024-12-10 11:59:07 +0800

    Refactor SparkOutputBox styling and simplify component structure
    - Updated the StyledBox component to use theme-based styling for better responsiveness.
    - Changed the CSS selectors to target MUI Card components instead of the previous output area.
    - Streamlined the return statement in SparkOutputBox for improved readability.

--- webapp/src/components/notebook/content/cell/result/SparkOutputBox.js
+++ webapp/src/components/notebook/content/cell/result/SparkOutputBox.js
@@ -1,23 +1,20 @@
 import { Box } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
-const StyledBox = styled(Box)({
+const StyledBox = styled(Box)(({ theme }) => ({
   width: '85%',
   marginTop: 0,
   marginBottom: 1,
   marginLeft: 40,
   marginRight: 1,
-  '& .jp-OutputArea-output': {
-    padding: '10px 0',
+  '& .MuiCard-root': {
+    marginLeft: 40,
+    width: '85%'
   }
-});
+}));
 
 function SparkOutputBox({ children }) {
-  return (
-    <StyledBox>
-      {children}
-    </StyledBox>
-  );
+  return <StyledBox>{children}</StyledBox>;
 }
 
 export default SparkOutputBox;
